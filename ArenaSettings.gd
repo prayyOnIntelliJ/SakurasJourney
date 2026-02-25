f6fd7191b1ae1d9bd8c302d716339c389c9c6f2b
prayy <122681300+prayyOnIntelliJ@users.noreply.github.com>
2026-02-25 15:43:06 +0100

    Added new functions to get the spawn object in the global script

--- ArenaSettings.gd
+++ ArenaSettings.gd
@@ -1,9 +1,22 @@
 extends Node
 
-var _difficultyLevel: int
+var _difficultyLevel: int = -1
+var _spawnObject : Node
 
 func setDifficultyLevel(level: int):
 	_difficultyLevel = level
 
 func getDifficultyLevel():
+	assert (_difficultyLevel != -1, "DifficultyLevel hasn't been set yet.")
+	
 	return _difficultyLevel
+
+func setSpawnObject(spawnObject: Node):
+	_spawnObject = spawnObject
+
+func getSpawnObject():
+	if (_spawnObject == null):
+		_spawnObject = get_tree().root.find_child("ObjectsInGame", true, false)
+	
+	assert(_spawnObject != null, "Couldn't find ObjectsInGame in the scene tree.")
+	return _spawnObject
